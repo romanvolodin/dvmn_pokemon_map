4820ddb58981968ad24a8ba06a1b60e01d5b7ffa
Roman Volodin <roman.volodin@gmail.com>
2021-09-17 15:57:31 +0300

    Change default values for PokemonEntity

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -13,13 +13,13 @@ class PokemonEntity(models.Model):
     pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
     latitude = models.FloatField()
     longitude = models.FloatField()
-    appeared_at = models.DateTimeField()
-    disappeared_at = models.DateTimeField()
-    level = models.IntegerField()
-    health = models.IntegerField()
-    strength = models.IntegerField()
-    defence = models.IntegerField()
-    stamina = models.IntegerField()
+    appeared_at = models.DateTimeField(null=True, blank=True)
+    disappeared_at = models.DateTimeField(null=True, blank=True)
+    level = models.IntegerField(default=0, blank=True)
+    health = models.IntegerField(default=0, blank=True)
+    strength = models.IntegerField(default=0, blank=True)
+    defence = models.IntegerField(default=0, blank=True)
+    stamina = models.IntegerField(default=0, blank=True)
 
     def __str__(self):
         return self.latitude, self.longitude
